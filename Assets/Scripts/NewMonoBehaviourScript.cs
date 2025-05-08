da8f4678a8859e5bbb5cceb182cd7241dd08b239
haktandincell <mehmethaktan55@gmail.com>
2025-05-08 18:24:36 +0300

    yedek3

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,29 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CinemachineRotationSwitcher : MonoBehaviour
+{
+    private CinemachineCamera vcam;
+   
+    private CinemachineRotationComposer rotationComposer;
+   
+    public void Start()
+    {
+        vcam = GetComponent<CinemachineCamera>();     
+        rotationComposer = vcam.GetComponent<CinemachineRotationComposer>();
+       
+        DisableAllRotationControls();
+    }
+
+    public void SwitchRotationMode(string mode)
+    {
+        DisableAllRotationControls();
+
+        
+    }
+
+    public void DisableAllRotationControls()
+    { 
+        if (rotationComposer != null) rotationComposer.enabled = false;     
+    }
+}
